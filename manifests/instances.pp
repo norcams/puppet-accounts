3b779b454c6cabc7adb1b5a47b9f814505238975
Jan Ivar Beddari <code@beddari.net>
2015-04-25 20:58:34 +0200

    Autolookup first, then fall back to hiera_hash

--- manifests/instances.pp
+++ manifests/instances.pp
@@ -11,12 +11,27 @@ class accounts::instances(
   $ssh_authorized_key_title = '%{ssh_key}-on-%{account}',
 ) {
 
+  if empty($groups) {
+    $groups_data = hiera_hash('accounts::groups', {}),
+  }
+  if empty($ssh_keys) {
+    $ssh_keys_data = hiera_hash('accounts::ssh_keys', {}),
+  }
+  if empty($users) {
+    $users_data = hiera_hash('accounts::users', {}),
+  }
+  if empty($usergroups) {
+    $usergroups_data = hiera_hash('accounts::usergroups', {}),
+  }
+  if empty($accounts) {
+    $accounts_data = hiera_hash('accounts::accounts', {}),
+  }
   class { 'accounts':
-    groups                   => hiera_hash('accounts::groups', {}),
-    ssh_keys                 => hiera_hash('accounts::ssh_keys', {}),
-    users                    => hiera_hash('accounts::users', {}),
-    usergroups               => hiera_hash('accounts::usergroups', {}),
-    accounts                 => hiera_hash('accounts::accounts', {}),
+    groups                   => $groups_data,
+    ssh_keys                 => $ssh_keys_data,
+    users                    => $users_data,
+    usergroups               => $usergroups_data,
+    accounts                 => $accounts_data,
     start_uid                => $start_uid,
     start_gid                => $start_gid,
     purge_ssh_keys           => $purge_ssh_keys,
